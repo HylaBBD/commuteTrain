6309d001f8cbe3979a95b289837d1ee2db71785d
reece-stephenson <reeces@bbd.co.za>
2023-07-25 20:48:05 +0200

    Testing Swagger on PROD

--- backend/src/main/java/com/commutetrip/backend/controllers/BookingController.java
+++ backend/src/main/java/com/commutetrip/backend/controllers/BookingController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import com.commutetrip.backend.database.entities.CommuterBookingEntity;
@@ -21,49 +23,42 @@ import com.commutetrip.backend.models.CommuterBooking;
 @Tag(name = "Bookings")
 @RestController
 @RequestMapping("api/commute-train/bookings")
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 public class BookingController {
-    private final CommuterBookingService commuterBookingService;
+        private final CommuterBookingService commuterBookingService;
 
-    @Operation(summary = "Get all Commuter Bookings", description = "Get an array of Commuter Bookings by either commuter id or truck route id")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
-    })
-    @GetMapping("")
-    public ResponseEntity<List<CommuterBooking>> getBookings(
-            @RequestParam(value = "commuterId", required = false)
-            @Parameter( name = "commuterId", description = "Id of Commuter", example = "1")
-            Long commuterId,
-            @RequestParam(value = "truckRouteId", required = false)
-            @Parameter(name = "truckRouteId", description = "Id of Truck Route", example = "1")
-            Long truckRouteId
-    ) {
-        return new ResponseEntity<>(commuterBookingService.findBookings(commuterId, truckRouteId), HttpStatus.OK);
-    }
+        @Operation(summary = "Get all Commuter Bookings", description = "Get an array of Commuter Bookings by either commuter id or truck route id")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+        })
+        @GetMapping("")
+        public ResponseEntity<List<CommuterBooking>> getBookings(
+                        @RequestParam(value = "commuterId", required = false) @Parameter(name = "commuterId", description = "Id of Commuter", example = "1") Long commuterId,
+                        @RequestParam(value = "truckRouteId", required = false) @Parameter(name = "truckRouteId", description = "Id of Truck Route", example = "1") Long truckRouteId) {
+                return new ResponseEntity<>(commuterBookingService.findBookings(commuterId, truckRouteId),
+                                HttpStatus.OK);
+        }
 
-    @Operation(summary = "Get a Commuter Bookings", description = "Get a Commuter Bookings by Booking Id")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
-            @ApiResponse(responseCode = "404", description = "Not Found"),
-    })
-    @GetMapping("/{bookingId}")
-    public ResponseEntity<CommuterBooking> getBooking(
-            @PathVariable("bookingId")
-            @Parameter( name = "bookingId", description = "Id of Booking", example = "1")
-            Long bookingId
-    ) {
-        return commuterBookingService.getBooking(bookingId)
-                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
+        @Operation(summary = "Get a Commuter Bookings", description = "Get a Commuter Bookings by Booking Id")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+                        @ApiResponse(responseCode = "404", description = "Not Found"),
+        })
+        @GetMapping("/{bookingId}")
+        public ResponseEntity<CommuterBooking> getBooking(
+                        @PathVariable("bookingId") @Parameter(name = "bookingId", description = "Id of Booking", example = "1") Long bookingId) {
+                return commuterBookingService.getBooking(bookingId)
+                                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        }
 
-    @Operation(summary = "Create new Booking", description = "Create a Commuter Booking")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Successfully Created"),
-    })
-    @PostMapping("")
-    public ResponseEntity<CommuterBooking> saveBooking(
-            @RequestBody CommuterBookingEntity booking
-    ) {
-        return new ResponseEntity<>(commuterBookingService.saveBooking(booking), HttpStatus.CREATED);
-    }
+        @Operation(summary = "Create new Booking", description = "Create a Commuter Booking")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "201", description = "Successfully Created"),
+        })
+        @PostMapping("")
+        public ResponseEntity<CommuterBooking> saveBooking(
+                        @RequestBody CommuterBookingEntity booking) {
+                return new ResponseEntity<>(commuterBookingService.saveBooking(booking), HttpStatus.CREATED);
+        }
 }
--- backend/src/main/java/com/commutetrip/backend/controllers/CommuterController.java
+++ backend/src/main/java/com/commutetrip/backend/controllers/CommuterController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import com.commutetrip.backend.services.CommuterService;
@@ -19,46 +21,41 @@ import com.commutetrip.backend.models.Commuter;
 @RequiredArgsConstructor
 @Tag(name = "Commuters")
 @RestController
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 @RequestMapping("api/commute-train/commuters")
 public class CommuterController {
-    private final CommuterService commuterService;
+        private final CommuterService commuterService;
 
-    @Operation(summary = "Get all Commuters", description = "Returns an array of Commuters")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
-    })
-    @GetMapping("")
-    public ResponseEntity<List<Commuter>> getAllCommuters() {
-        return new ResponseEntity<>(commuterService.findAllCommuters(), HttpStatus.OK);
-    }
+        @Operation(summary = "Get all Commuters", description = "Returns an array of Commuters")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+        })
+        @GetMapping("")
+        public ResponseEntity<List<Commuter>> getAllCommuters() {
+                return new ResponseEntity<>(commuterService.findAllCommuters(), HttpStatus.OK);
+        }
 
-    @Operation(summary = "Get a Commuter", description = "Get a Commuter by either Name or Id")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
-            @ApiResponse(responseCode = "404", description = "Not Found"),
-    })
-    @GetMapping("/commuter")
-    public ResponseEntity<Commuter> getCommuter(
-            @RequestParam(value = "name", required = false)
-            @Parameter( name = "name", description = "Name of Commuter", example = "Jeff Surname")
-            String name,
-            @RequestParam(value = "id", required = false)
-            @Parameter(name = "id", description = "Id of Commuter", example = "1")
-            Long id
-    ) {
-        return commuterService.findCommuter(name, id)
-                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
+        @Operation(summary = "Get a Commuter", description = "Get a Commuter by either Name or Id")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+                        @ApiResponse(responseCode = "404", description = "Not Found"),
+        })
+        @GetMapping("/commuter")
+        public ResponseEntity<Commuter> getCommuter(
+                        @RequestParam(value = "name", required = false) @Parameter(name = "name", description = "Name of Commuter", example = "Jeff Surname") String name,
+                        @RequestParam(value = "id", required = false) @Parameter(name = "id", description = "Id of Commuter", example = "1") Long id) {
+                return commuterService.findCommuter(name, id)
+                                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        }
 
-    @Operation(summary = "Create new Commuter", description = "Create a Commuter")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Successfully Created"),
-    })
-    @PostMapping("")
-    public ResponseEntity<Commuter> saveCommuter(
-            @RequestBody Commuter commuter
-    ) {
-        return new ResponseEntity<>(commuterService.saveCommuter(commuter), HttpStatus.CREATED);
-    }
+        @Operation(summary = "Create new Commuter", description = "Create a Commuter")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "201", description = "Successfully Created"),
+        })
+        @PostMapping("")
+        public ResponseEntity<Commuter> saveCommuter(
+                        @RequestBody Commuter commuter) {
+                return new ResponseEntity<>(commuterService.saveCommuter(commuter), HttpStatus.CREATED);
+        }
 }
